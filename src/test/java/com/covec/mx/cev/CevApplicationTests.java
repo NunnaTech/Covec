c3b84b822b3e22323d50ed7361f1154c66f6e72e
Raul <sterplay2010@gmail.com>
2022-03-05 13:50:50 -0600

    Proyecto

---
+++ src/test/java/com/covec/mx/cev/CevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covec.mx.cev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
