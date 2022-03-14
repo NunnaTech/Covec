d1b0723193fd9273b70aad9af694957c2819159d
raulpc <sterplay2010@gmail.com>
2022-03-14 09:52:37 -0600

    LOL

--- src/main/java/com/covec/mx/cev/entities/categoria/CategoriaController.java
+++ src/main/java/com/covec/mx/cev/entities/categoria/CategoriaController.java
@@ -1,5 +1,16 @@
 package com.covec.mx.cev.entities.categoria;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categorias")
 public class CategoriaController {
-    
+
+    @GetMapping("/all")
+    public String allCategories(){
+        return "categoria/categoriacrud";
+    }
+
 }
