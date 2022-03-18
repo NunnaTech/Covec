b35986d7345eb9706d590ca9ff7d064ef06002db
unknown <palma7876@gmail.com>
2022-03-18 12:41:04 -0600

    Crud Municipio

--- src/main/java/com/covec/mx/cev/entities/municipio/MunicipioController.java
+++ src/main/java/com/covec/mx/cev/entities/municipio/MunicipioController.java
@@ -1,5 +1,54 @@
 package com.covec.mx.cev.entities.municipio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/municipios")
 public class MunicipioController {
-    
+
+    @Autowired
+    private MunicipioService municipioService;
+
+    @GetMapping("/all")
+    public String allMunicipios(@RequestParam Map<String,Object> params, Model model){
+        int page = params.get("page") != null ? (Integer.valueOf(params.get("page").toString()) - 1) : 0;
+        PageRequest pageRequest = PageRequest.of(page,5);
+        Page<Municipio> municipioPage = municipioService.getAll(pageRequest);
+        int totalPages = municipioPage.getTotalPages();
+        if (totalPages>0){
+            List<Integer> pages = IntStream.rangeClosed(1,totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("paginas",pages);
+        }
+        model.addAttribute("municipio",new Municipio());
+        model.addAttribute("municipios",municipioPage.getContent());
+        return "municipio/municipiocrud";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("municipio") Municipio municipio){
+        municipioService.save(municipio);
+        return "redirect:/municipios/all";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("municipio") Municipio municipio){
+        municipioService.update(municipio);
+        return "redirect:/municipios/all";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        municipioService.delete(id);
+        return "redirect:/municipios/all";
+    }
 }
--- src/main/java/com/covec/mx/cev/entities/municipio/MunicipioService.java
+++ src/main/java/com/covec/mx/cev/entities/municipio/MunicipioService.java
@@ -1,5 +1,46 @@
 package com.covec.mx.cev.entities.municipio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class MunicipioService {
-    
+    @Autowired
+    private MunicipioRepository municipioRepository;
+
+    public Page<Municipio> getAll(Pageable pageable){
+        return municipioRepository.findAll(pageable);
+    }
+
+    public Municipio getOne(int id){
+        Optional<Municipio> exist = municipioRepository.findById(id);
+        if (exist.isPresent()){
+            Municipio municipio = new Municipio();
+            municipio = exist.get();
+            return municipio;
+        }
+        return null;
+    }
+    public Municipio save(Municipio municipio){
+        municipioRepository.save(municipio);
+        return municipio;
+    }
+
+    public Municipio update(Municipio newObject){
+        Optional<Municipio> exist = Optional.empty();
+        exist = municipioRepository.findById(newObject.getId());
+        if (!exist.isEmpty())
+            municipioRepository.save(newObject);
+        return exist.get();
+    }
+    public void delete(int id){
+        boolean exist = municipioRepository.existsById(id);
+        if (exist) {
+            municipioRepository.deleteById(id);
+        }
+    }
 }
