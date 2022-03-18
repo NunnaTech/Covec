6974ccf3bf1b2eb8b0359e82dbb05fd375fb49d0
jairDavid <jairvasmar@gmail.com>
2022-03-18 10:22:30 -0600

    some bug fixes

--- src/main/java/com/covec/mx/cev/CevApplication.java
+++ src/main/java/com/covec/mx/cev/CevApplication.java
@@ -1,14 +1,9 @@
 package com.covec.mx.cev;
 
-import com.covec.mx.cev.entities.usuario.enlace.Enlace;
-import com.covec.mx.cev.entities.usuario.enlace.EnlaceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import java.util.List;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CevApplication {
