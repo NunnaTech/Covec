53abab1f8efa53cd86fac0ab035219eda8fb146a
unknown <palma7876@gmail.com>
2022-03-23 00:38:13 -0600

    Dependencia Config propeties

--- src/main/java/com/covec/mx/cev/entities/colonia/ColoniaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.covec.mx.cev.entities.colonia;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ColoniaRepository extends CrudRepository<Colonia,Integer>{
-    
-}
--- src/main/java/com/covec/mx/cev/entities/municipio/MunicipioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.covec.mx.cev.entities.municipio;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MunicipioRepository extends CrudRepository<Municipio,Integer> {
-    
-}
